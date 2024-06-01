2cedd296c32378e4f2bdcecc7c409c94641f2569
Divyanshu Pathak <135452280+Divyanshu800@users.noreply.github.com>
2024-06-02 02:54:31 +0530

    added dynamic routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { userExists, userNotExists } from "./redux/reducers/auth";
 import { Toaster } from "react-hot-toast";
 import { SocketProvider } from "./socket";
 
+// Dynamic Routing
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
@@ -36,6 +37,7 @@ const App = () => {
   }, [dispatch]);
  
 
+  // if loader is true then show loading else show the page
   return loader ? (
     <LayoutLoader />
   ) : (

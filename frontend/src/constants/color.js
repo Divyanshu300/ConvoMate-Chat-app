78fdb75a74bc97f2145b7ea76ec3f5aa3b9a7ded
Divyanshu Pathak <135452280+Divyanshu800@users.noreply.github.com>
2024-06-02 02:33:23 +0530

    Added colors in color.js

--- frontend/src/constants/color.js
+++ frontend/src/constants/color.js
@@ -1,5 +1,5 @@
 export const bgBlue = "#164863";
-export const bgBlueLight = "rgb(66, 125, 157, 0.2)";
+export const bgBlueLight = "rgba(66, 125, 157, 0.2)";
 
 // export const grayColor = "rgba(247,247,247,1)";
 export const grayColor = "#DDF2FD";
@@ -7,5 +7,5 @@ export const lightBlue = "#2694ab";
 export const matBlack = "#1c1c1c";
 export const bgGradient = "linear-gradient(rgb(22, 72, 99), rgb(66, 125, 157))";
 
-export const purple = "rgba(75,12,192,1)";
-export const purpleLight = "rgba(75,12,192,0.2)";
+export const purple = "rgba(28, 22, 120, 1)";
+export const purpleLight = "rgba(133, 118, 255, 0.2)";

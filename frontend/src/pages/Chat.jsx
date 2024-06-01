9c936adf7b6f4a35da71fce685cdfbeb59fd88e6
Divyanshu Pathak <135452280+Divyanshu800@users.noreply.github.com>
2024-06-02 01:46:24 +0530

    Updated Chat.jsx with React UI code

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 import AppLayout from "../components/layout/AppLayout";
 import { IconButton, Skeleton, Stack } from "@mui/material";
-import { grayColor, orange } from "../constants/color";
+import { grayColor, bgBlue } from "../constants/color";
 import {
   AttachFile as AttachFileIcon,
   Send as SendIcon,
@@ -179,4 +179,83 @@ const Chat = ({ chatId, user }) => {
 
   const allMessages = [...oldMessages, ...messages];
 
-  
+  return chatDetails.isLoading ? (
+    <Skeleton />
+  ) : (
+    <Fragment>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      >
+        {allMessages.map((i) => (
+          <MessageComponent key={i._id} message={i} user={user} />
+        ))}
+
+        {userTyping && <TypingLoader />}
+
+        <div ref={bottomRef} />
+      </Stack>
+
+      <form
+        style={{
+          height: "10%",
+        }}
+        onSubmit={submitHandler}
+      >
+        <Stack
+          direction={"row"}
+          height={"100%"}
+          padding={"1rem"}
+          alignItems={"center"}
+          position={"relative"}
+        >
+          <IconButton
+            sx={{
+              position: "absolute",
+              left: "1.5rem",
+              rotate: "30deg",
+            }}
+            onClick={handleFileOpen}
+          >
+            <AttachFileIcon />
+          </IconButton>
+
+          <InputBox
+            placeholder="Type Message Here..."
+            value={message}
+            onChange={messageOnChange}
+          />
+
+          <IconButton
+            type="submit"
+            sx={{
+              rotate: "-30deg",
+              bgcolor: bgBlue,
+              color: "white",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover": {
+                bgcolor: "error.dark",
+              },
+            }}
+          >
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </form>
+
+      <FileMenu anchorE1={fileMenuAnchor} chatId={chatId} />
+    </Fragment>
+  );
+};
+
+export default AppLayout()(Chat);
+
